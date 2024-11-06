e1389b44df047e5e32ea861a45987325a64cdc21
KeenGWatanabe <mckeenweb@gmail.com>
2024-11-07 07:19:19 +0800

    r

---
+++ src/routes/About.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const About =()=>{
+      return (
+            <div>
+                  <li>This is About page</li>
+                  
+            </div>
+      )
+}
+export default About;
---
+++ src/routes/Careers.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const Careers =()=>{
+      return (
+            <div>
+                  <li>This is Careers page</li>
+                  
+            </div>
+      )
+}
+export default Careers;
